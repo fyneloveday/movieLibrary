b56f396b7c246144a447c76fde70731485aeca2f
fyneloveday <jbfyne18@yahoo.com>
2019-07-29 12:50:00 -0500

    Changed Get method from IEnumerable to List<>, then created a constructor with the list of movies, including seeded items. Changed Get method return to return movie

--- movieLibrary/movieLibrary/Controllers/MovieController.cs
+++ movieLibrary/movieLibrary/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using movieLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,10 +10,24 @@ namespace movieLibrary.Controllers
 {
     public class MovieController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        private List<Movie> movie = new List<Movie>();
+
+        public MovieController()
         {
-            return new string[] { "movie1", "movie2", "movie3", "movie4", "movie5" };
+            movie.Add (new Movie { Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" });
+            movie.Add (new Movie { Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" });
+            movie.Add (new Movie { Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" });
+            movie.Add (new Movie { Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" });
+            movie.Add (new Movie { Title = "Die Hard", Genre = "Action", Director = "John McTiernan" });
+        }
+
+
+        // GET api/<controller>
+        //public IEnumerable<string> Get()
+        public List<Movie> Get()
+        {
+            return movie;
+            //return new string[] { "movie1", "movie2", "movie3", "movie4", "movie5" };
         }
 
         // GET api/<controller>/5
