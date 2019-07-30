cb647fa555c324e31cfa063dc95d936554a55606
fyneloveday <jbfyne18@yahoo.com>
2019-07-30 16:55:43 -0500

    Created IQueryable GetMovie method for list of movies

--- movieLibrary/movieLibrary/Controllers/MovieController.cs
+++ movieLibrary/movieLibrary/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using movieLibrary.Models;
+﻿using movieLibrary.CorFile;
+using movieLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,38 +12,53 @@ namespace movieLibrary.Controllers
 {
     public class MovieController : ApiController
     {
-        private List<Movie> movie = new List<Movie>();
 
-        public MovieController()
+        //[AllowCrossSite]
+        //[RoutePrefix("api/Movies")]
+        //public class MoviesController : ApiController
+        //{
+        //    private ApplicationDbContext db = new ApplicationDbContext();
+
+        //    // GET: api/Movies
+        //    [Route("")]
+        //    public IQueryable<Movie> GetMovies()
+        //    {
+        //        return db.movie;
+        //    }
+
+
+            //private List<Movie> movie = new List<Movie>();
+
+            //public MovieController()
+            //{
+            //    movie.Add (new Movie { Id = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" });
+            //    movie.Add (new Movie { Id = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" });
+            //    movie.Add (new Movie { Id = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" });
+            //    movie.Add (new Movie { Id = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" });
+            //    movie.Add (new Movie { Id = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" });
+            //}
+
+
+            // GET api/<controller>
+            public IEnumerable<string> Get()
+            //public List<Movie> Get()
         {
-            movie.Add (new Movie { Id = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" });
-            movie.Add (new Movie { Id = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" });
-            movie.Add (new Movie { Id = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" });
-            movie.Add (new Movie { Id = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" });
-            movie.Add (new Movie { Id = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" });
-        }
-
-
-        // GET api/<controller>
-        //public IEnumerable<string> Get()
-        public List<Movie> Get()
-        {
-            return movie;
-            //return new string[] { "movie1", "movie2", "movie3", "movie4", "movie5" };
+            //return movie;
+            return new string[] { "movie1", "movie2", "movie3", "movie4", "movie5" };
         }
 
         // GET api/<controller>/5
-        public Movie Get(int id)
-        {
-            return movie.Where(m => m.Id == id).FirstOrDefault();
-            //return "value";
-        }
+        //public Movie Get(int id)
+        //{
+        //    return movie.Where(m => m.Id == id).FirstOrDefault();
+        //    //return "value";
+        //}
 
         // POST api/<controller>      
-        public void Post(Movie value)
-        {
-            movie.Add(value);                        
-        }
+        //public void Post(Movie value)
+        //{
+        //    movie.Add(value);                        
+        //}
 
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value)
--- movieLibrary/movieLibrary/CorFile/AllowCrossSiteAttribute.cs
+++ movieLibrary/movieLibrary/CorFile/AllowCrossSiteAttribute.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Filters;
 
 namespace movieLibrary.CorFile
 {
-    public class CorsAttributes
+    public class AllowCrossSiteAttribute : System.Web.Http.Filters.ActionFilterAttribute
     {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Response != null)
+                actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
     }
 }
