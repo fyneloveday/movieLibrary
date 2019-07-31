eaad9b48cf7abf3edf2cd20a1acb26c517823676
fyneloveday <jbfyne18@yahoo.com>
2019-07-31 12:59:55 -0500

    Created FileController, added an index method

--- movieLibrary/movieLibrary/Controllers/MovieController.cs
+++ movieLibrary/movieLibrary/Controllers/MovieController.cs
@@ -30,35 +30,14 @@ namespace movieLibrary.Controllers
                 return db.movie;
             }
 
-
-            //private List<Movie> movie = new List<Movie>();
-
-            //public MovieController()
-            //{
-            //    movie.Add (new Movie { Id = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" });
-            //    movie.Add (new Movie { Id = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" });
-            //    movie.Add (new Movie { Id = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" });
-            //    movie.Add (new Movie { Id = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" });
-            //    movie.Add (new Movie { Id = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" });
-            //}
-
-
-            //// GET api/<controller>
-            //public IEnumerable<string> Get()
-            ////public List<Movie> Get()
-            //{
-            //    //return movie;
-            //    return new string[] { "movie1", "movie2", "movie3", "movie4", "movie5" };
-            //}
-
-
+                        
             // GET: api/Movies/5
             [Route("{id:int}")]
             [HttpGet]
             [ResponseType(typeof(Movie))]
             public IHttpActionResult GetMovie(int id)
             {
-                Movie movie = db.movie.Where(m => m.Id == id).SingleOrDefault();
+                Movie movie = db.movie.Include(m => m.Id == id).SingleOrDefault();
                 if (movie == null)
                 {
                     return NotFound();
@@ -72,21 +51,21 @@ namespace movieLibrary.Controllers
             [Route("Title/{title}")]
             public IQueryable<Movie> GetMoviesByTitle(string title)
             {
-                return db.movie.Where(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+                return db.movie.Include(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
             }
 
             // GET: api/Movies/Genre
             [Route("Genre/{genre}")]
             public IQueryable<Movie> GetMoviesByGenre(string genre)
             {
-                return db.movie.Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase));
+                return db.movie.Include(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase));
             }
 
             // GET: api/Movies/Director
             [Route("Director/{name}")]
             public IQueryable<Movie> GetMoviesByDirectorName(string name)
             {
-                return db.movie.Where(b => b.Director.Equals(name, StringComparison.OrdinalIgnoreCase));
+                return db.movie.Include(b => b.Director.Equals(name, StringComparison.OrdinalIgnoreCase));
             }
 
 
